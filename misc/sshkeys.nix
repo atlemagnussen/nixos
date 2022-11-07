eba8172a375d3b26bb93d5c206789eea78277553
Atle Magnussen <atlemagnussen@gmail.com>
2022-11-07 10:13:08 +0100

    update key

--- misc/sshkeys.nix
+++ misc/sshkeys.nix
@@ -1,7 +1,7 @@
 {
     trustedKeys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM28zTRnAGUTUTEoo4G6sOLVqu7CnZu29dhk+6zyCCOd atle@atle-laptop"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBmSfC7UYFcvvRWVM9hxAGSwqYDGCwj29Aeh2kOym+ih atle@ATLE-WORKTOP"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIeE1WzbrF1xhODV6ljdBopQDDxVfxiT+qKfEYcNwiOn atle@ATLE-WORKTOP"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILMpbRg4CMsacIa67T2c7uvz1loaMRfkUJK06bRx1l0h atle@Atles-MacBook-Air.local"
     ];
 }
