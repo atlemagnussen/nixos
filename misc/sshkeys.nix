35646954c76e917cb91ba95f978c3f3b1c1001cd
Atle Magnussen <atlemagnussen@gmail.com>
2022-11-07 11:51:26 +0100

    add windows ssh

--- misc/sshkeys.nix
+++ misc/sshkeys.nix
@@ -2,6 +2,7 @@
     trustedKeys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM28zTRnAGUTUTEoo4G6sOLVqu7CnZu29dhk+6zyCCOd atle@atle-laptop"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIeE1WzbrF1xhODV6ljdBopQDDxVfxiT+qKfEYcNwiOn atle@ATLE-WORKTOP"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE0pckBo3uDm1niG73/PT8mdXIhZz+TcDMFR/ZTnWDe8 atlem@atle-worktop"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILMpbRg4CMsacIa67T2c7uvz1loaMRfkUJK06bRx1l0h atle@Atles-MacBook-Air.local"
     ];
 }
