977ba67e7fb49e679341627b63d432ac38417db7
Atle Magnussen <atlemagnussen@gmail.com>
2022-11-16 09:17:36 +0100

    try home from example

--- config/home.nix
+++ config/home.nix
@@ -1,26 +1,40 @@
-{ config, pkgs, lib, l, vars, ... }:
+{ config, pkgs, ... }:
+
 {
- 
-  programs = {
-    ssh = {
-      enable = true;
-    };
-    home-manager.enable = true;
-  };
-  home = {
-    stateVersion = "22.05";
-    username = "atle";
-    homeDirectory = "/home/atle";
-    sessionVariables = {
-      NPM_PACKAGES = "$HOME/.npm-packages";
-    };
-    programs = {
-      bash = {
-        enable = true;
-        initExtra = ''
-          npm set prefix $HOME/.npm-packages
-        '';
-      };
-    };
-  };
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "atle";
+  home.homeDirectory = "/home/atle";
+
+  # Packages that should be installed to the user profile.
+  home.packages = [                               1
+    pkgs.fortune
+  ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+#   programs.emacs = {                              2
+#     enable = true;
+#     extraPackages = epkgs: [
+#       epkgs.nix-mode
+#       epkgs.magit
+#     ];
+#   };
+
+#   services.gpg-agent = {                          3
+#     enable = true;
+#     defaultCacheTtl = 1800;
+#     enableSshSupport = true;
+#   };
 }
