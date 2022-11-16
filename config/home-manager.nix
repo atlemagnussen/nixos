0d4d28d0cd0276e2797b56c685a811872190d81e
Atle Magnussen <atlemagnussen@gmail.com>
2022-11-16 13:52:36 +0100

    try some moer

--- config/home-manager.nix
+++ config/home-manager.nix
@@ -9,6 +9,17 @@
 
   home-manager.users.atle = { pkgs, ... }: {
     home.packages = [ pkgs.fortune ];
-    programs.bash.enable = true;
+    programs.bash = {
+      enable = true;
+      initExtra = ''
+          npm set prefix $HOME/.npm-packages
+        '';
+    };
+
+    programs.git = {
+      enable = true;
+      userName  = "Atle Magnussen";
+      userEmail = "atlemagnussen@gmail.com";
+    };
   };
 }
