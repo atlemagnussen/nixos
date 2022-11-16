4a8897ca745cfbb9953f3444d4fa1d68aa4c44b3
Atle Magnussen <atlemagnussen@gmail.com>
2022-11-16 09:56:23 +0100

    try

--- config/home.nix
+++ config/home.nix
@@ -3,8 +3,12 @@
 {
   imports = [ <home-manager/nixos> ];
 
+  programs = {
+    home-manager.enable = true;
+  };
+
   home-manager.users.atle = { pkgs, ... }: {
-    home.packages = [ pkgs.fortune  pkgs.httpie ];
+    home.packages = [ pkgs.fortune ];
     programs.bash.enable = true;
     };
 }
