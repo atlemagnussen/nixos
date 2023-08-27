193dbcbe9fe06337c83316e66a9a15c2b3f39a91
Atle Magnussen <atlemagnussen@gmail.com>
2023-08-27 13:21:04 +0200

    try bridge again

--- machines/atle-station/network-configuration.nix
+++ machines/atle-station/network-configuration.nix
@@ -1,17 +1,35 @@
 # atle-station netwok
 { config, pkgs, lib, ... }:
+let
+  bridge_interface = "br0";
+  lan_interface = "enp4s0";
+in
 {
   networking = {
     hostName = "atle-station";
+    networkmanager.enable = true;
+    useDHCP = false #lib.mkDefault true;
+    firewall.enable = false;
+    interfaces = {
+      ${lan_interface}.useDHCP = false;
+      ${bridge_interface}.useDHCP = true;
+    };
+    bridges = {
+      ${bridge_interface} = {
+        interfaces = [
+          lan_interface
+        ];
+      };
+    };
   };
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
+  
   # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
 
   
@@ -19,7 +37,6 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
 
   #networking.bridges = {
   #  "br0" = {
