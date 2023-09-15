8d779564bf5bb0e6ac213549e27c7b6a59d76845
Atle Magnussen <atlemagnussen@gmail.com>
2023-09-15 09:55:50 +0200

    ds

--- machines/atle-station/systemd.nix
+++ machines/atle-station/systemd.nix
@@ -1,26 +1,26 @@
 
-{ config, pkgs, ... }:
+# { config, pkgs, ... }:
 
-{
-  systemd.timers."hello-world" = {
-  wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnBootSec = "5m";
-      OnUnitActiveSec = "5m";
-      Unit = "hello-world.service";
-    };
-};
+# {
+#   systemd.timers."hello-world" = {
+#   wantedBy = [ "timers.target" ];
+#     timerConfig = {
+#       OnBootSec = "5m";
+#       OnUnitActiveSec = "5m";
+#       Unit = "hello-world.service";
+#     };
+# };
 
-systemd.services."hello-world" = {
-  script = ''
-    set -eu
-    ${pkgs.coreutils}/bin/echo "Hello World"
-  '';
-  serviceConfig = {
-    Type = "oneshot";
-    User = "root";
-  };
-};
+# systemd.services."hello-world" = {
+#   script = ''
+#     set -eu
+#     ${pkgs.coreutils}/bin/echo "Hello World"
+#   '';
+#   serviceConfig = {
+#     Type = "oneshot";
+#     User = "root";
+#   };
+# };
 
-}
+# }
 
