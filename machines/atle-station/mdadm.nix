e65bdbb0b592055a05a6032143c1daed49bc7e6e
Atle Magnussen <atlemagnussen@gmail.com>
2024-05-05 12:51:09 +0200

    try ch

--- machines/atle-station/mdadm.nix
+++ machines/atle-station/mdadm.nix
@@ -2,14 +2,14 @@
 let
   mdadmconf = ''
     ARRAY /dev/md/atle-station:1 metadata=1.2 name=atle-station:1 UUID=067378a8:3b31e8ab:42cbe7a3:e15a4a84
-    ARRAY /dev/md/atle-station:0 metadata=1.2 name=atle-station:0 UUID=69e33663:d1a088c1:ac0a7739:1d05b0d7
+    ARRAY /dev/md/atle-pc:0 metadata=1.2 name=atle-pc:0 UUID=69e33663:d1a088c1:ac0a7739:1d05b0d7
   '';
 in
 {
   systemd.services.mdadm-monitor = {
     description = "Monitor RAID disks";
     wantedBy = [ "multi-user.target" ];
-    script = "${pkgs.mdadm}/bin/mdadm --monitor /dev/md/atle-station:0";
+    script = "${pkgs.mdadm}/bin/mdadm --monitor /dev/md/atle-pc:0";
   };
   environment.etc.mdadmconf = {
     target = "mdadm/mdadm.conf";
@@ -19,18 +19,19 @@ in
       ${mdadmconf}
     '';
   };
+
   boot.swraid.enable = true;
   boot.swraid.mdadmConf = mdadmconf;
 
-  #fileSystems = {
-  #  "/mnt/md0" = {
-  #    device = "/dev/md/atle-station:0";
-  #    fsType = "ext4";
-  #  };
-  #  "/mnt/md1" = {
-  #    device = "/dev/md/atle-station:1";
-  #    fsType = "ext4";
-  #  };
-  #};
+  fileSystems = {
+    "/mnt/md0" = {
+      device = "/dev/md/atle-pc:0";
+      fsType = "ext4";
+    };
+    "/mnt/md1" = {
+      device = "/dev/md/atle-station:1";
+      fsType = "ext4";
+    };
+  };
   # boot.initrd.mdadmConf = mdadmconf;
 }
