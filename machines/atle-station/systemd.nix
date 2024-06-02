f0b7f9d7e7f66dc4293493fd6dde3382dc9a8c63
Atle Magnussen <atlemagnussen@gmail.com>
2024-06-02 14:08:33 +0200

    try ad no sleep

--- machines/atle-station/systemd.nix
+++ machines/atle-station/systemd.nix
@@ -1,6 +1,28 @@
 
 # { config, pkgs, ... }:
 
+systemd = {
+    targets = {
+        sleep = {
+            enable = false;
+            unitConfig.DefaultDependencies = "no";
+        };
+        suspend = {
+            enable = false;
+            unitConfig.DefaultDependencies = "no";
+        };
+        hibernate = {
+            enable = false;
+            unitConfig.DefaultDependencies = "no";
+        };
+        "hybrid-sleep" = {
+            enable = false;
+            unitConfig.DefaultDependencies = "no";
+        };
+    };
+};
+
+
 # {
 #   systemd.timers."hello-world" = {
 #   wantedBy = [ "timers.target" ];
