8941eb4d879fdd89ec0354b2433d2c98bc382a7c
Atle Magnussen <atlemagnussen@gmail.com>
2024-06-05 00:13:14 +0200

    try again

--- machines/atle-station/systemd.nix
+++ machines/atle-station/systemd.nix
@@ -3,31 +3,31 @@
 
 {
     systemd = {
+        sleep.extraConfig = ''
+            AllowSuspend=no
+            AllowHibernation=no
+            AllowHybridSleep=no
+            AllowSuspendThenHibernate=no
+        '';
 
-        targets = {
-            sleep = {
-                enable = false;
-                unitConfig.DefaultDependencies = "no";
-                extraConfig = ''
-                  AllowSuspend=no
-                  AllowHibernation=no
-                  AllowHybridSleep=no
-                  AllowSuspendThenHibernate=no
-                '';
-            };
-            suspend = {
-                enable = false;
-                unitConfig.DefaultDependencies = "no";
-            };
-            hibernate = {
-                enable = false;
-                unitConfig.DefaultDependencies = "no";
-            };
-            "hybrid-sleep" = {
-                enable = false;
-                unitConfig.DefaultDependencies = "no";
-            };
-        };
+        # targets = {
+        #     sleep = {
+        #         enable = false;
+        #         unitConfig.DefaultDependencies = "no";
+        #     };
+        #     suspend = {
+        #         enable = false;
+        #         unitConfig.DefaultDependencies = "no";
+        #     };
+        #     hibernate = {
+        #         enable = false;
+        #         unitConfig.DefaultDependencies = "no";
+        #     };
+        #     "hybrid-sleep" = {
+        #         enable = false;
+        #         unitConfig.DefaultDependencies = "no";
+        #     };
+        # };
     };
 }
 # {
