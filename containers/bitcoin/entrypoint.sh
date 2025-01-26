442174bca237035adc448ae1144ca0b583ce8345
Atle Magnussen <atlemagnussen@gmail.com>
2025-01-26 20:47:37 +0100

    shit

--- containers/bitcoin/entrypoint.sh
+++ containers/bitcoin/entrypoint.sh
@@ -12,5 +12,10 @@ fi
 
 echo "$0: assuming uid:gid for bitcoin:bitcoin of $(id -u bitcoin):$(id -g bitcoin)"
 
+if [ "$1" = "bitcoind" ] || [ "$1" = "bitcoin-cli" ] || [ "$1" = "bitcoin-tx" ]; then
+  echo
+  exec gosu bitcoin "$@"
+fi
+
 echo
 exec "$@"
