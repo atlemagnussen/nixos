5dfc62c603e0b8d67b959a2896fb2118f4e3923c
Atle Magnussen <atlemagnussen@gmail.com>
2025-01-26 21:27:14 +0100

    idk

--- containers/bitcoin/run.sh
+++ containers/bitcoin/run.sh
@@ -4,4 +4,4 @@ docker run -it \
   --user 1006:1001 \
  -p 8333:8333 --name bitcoind-atle \
  --mount type=bind,source=/mnt/ssd2/blockchain/bitcoin,target=/home/bitcoin/.bitcoin \
- atlmag/bitcoin bash
+ atlmag/bitcoin
