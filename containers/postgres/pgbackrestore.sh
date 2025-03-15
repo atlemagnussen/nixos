cbf9dd5090aee81a53f5883eeca401e81d67f72f
Atle Magnussen <atlemagnussen@gmail.com>
2025-03-15 11:14:31 +0100

    f

--- containers/postgres/pgbackrestore.sh
+++ containers/postgres/pgbackrestore.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 echo "pgback rest"
 sleep 2
-# pgbackrest --config=/mnt/pgbackrest.conf --stanza=main --log-level-console=info restore
+
+cat /mnt/pgbackrest.conf
+
+pgbackrest --config=/mnt/pgbackrest.conf --stanza=main --log-level-console=info restore
 echo "done"
 cat /mnt/pgbackrest.conf
 exit 0
