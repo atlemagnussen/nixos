a59b089ff85c64cfc24c150084e2daaf0322d870
Atle Magnussen <atlemagnussen@gmail.com>
2025-03-15 11:11:48 +0100

    f

--- containers/postgres/build.sh
+++ containers/postgres/build.sh
@@ -5,7 +5,7 @@ podman build --tag atlmag/postgres:17 .
 podman rmi atlmag/pgbackrest:latest
 podman build -f ContainerfilePgBackRest --tag atlmag/pgbackrest:latest .
 
-podman run--name pgbackrest-atle \
+podman run --name pgbackrest-atle \
 -v /mnt/md0/databases/postgres:/var/lib/postgresql/data \
 -v /mnt/md1/bak/postgres:/mnt \
 atlmag/pgbackrest:latest
--- containers/postgres/pgbackrestore.sh
+++ containers/postgres/pgbackrestore.sh
@@ -3,4 +3,5 @@ echo "pgback rest"
 sleep 2
 # pgbackrest --config=/mnt/pgbackrest.conf --stanza=main --log-level-console=info restore
 echo "done"
+cat /mnt/pgbackrest.conf
 exit 0
