73d91a970bd542549c6e82cc5944dee6c5adb2e1
Atle Magnussen <atlemagnussen@gmail.com>
2025-03-15 22:37:54 +0100

    try now

--- containers/postgres/pgbackrestore.sh
+++ containers/postgres/pgbackrestore.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 echo "pgback rest"
 whoami
-sleep 2
 
 cat /mnt/pgbackrest.conf
 
-pgbackrest --config=/mnt/pgbackrest.conf --stanza=main --log-level-console=info restore
+gosu postgres pgbackrest --config=/mnt/pgbackrest.conf --stanza=main --log-level-console=info restore
+
+#pgbackrest --config=/mnt/pgbackrest.conf --stanza=main --log-level-console=info restore
+
+
 echo "done"
 cat /mnt/pgbackrest.conf
 exit 0
